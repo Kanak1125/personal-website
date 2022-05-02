fd01c258742dbe48f85c2387bd34b79b1193e54c
Kanak-dev <kanak24.fv@gmail.com>
2022-05-02 19:38:27 +0545

    dark mode added and other styles

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ window.addEventListener("scroll", () => {
     else {
         header.classList.remove("sticky");
     }
-    ; // toggles the classList to sticky when the scroll to Y is greater than 80px...
+
+    // toggles the classList to sticky when the scroll to Y is greater than 80px...
 })
 
 //toggle nav-link style when clicked on menu-btn
@@ -18,3 +19,43 @@ menuBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     menuBtn.classList.toggle('animation');
 })
+
+const toggleTheme = document.querySelector('.light_dark_theme');
+toggleTheme.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+
+    if(toggleTheme.classList.contains('fa-moon')){
+        toggleTheme.classList.remove('fa-moon');
+        toggleTheme.classList.add('fa-sun');
+    }
+
+    else {
+        toggleTheme.classList.remove('fa-sun');
+        toggleTheme.classList.add('fa-moon');
+    }
+});
+
+//hover eventListener for services section..
+const services = document.querySelector('.service_lists');
+const servicesChildren = services.querySelectorAll('div');
+console.log(servicesChildren)
+
+servicesChildren.forEach(e => {
+        
+    e.addEventListener('mouseover', index => {
+        let serviceSvgs = document.querySelector('.service_svgs');
+        serviceSvgs.setAttribute('color', 'var(--primary-color)');
+        serviceSvgs.style.transition = 'var(--transition)';
+        console.log(element.target);
+    })
+
+    e.addEventListener('mouseout', () => {
+        serviceSvgs.removeAttribute('color');
+    })
+})
+
+// services.forEach(element => {
+//     element.addEventListener('mouseover', () => {
+//         serviceSvgs.setAttribute('color', 'red');
+//     });
+// });
